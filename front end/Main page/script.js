bcc59d790e18f2be629cbc902054392ef329163e
Shin Ryujin <43638986+ShinRyujin03@users.noreply.github.com>
2024-05-26 16:15:17 +0700

    main and example page added

---
+++ front end/Main page/script.js
@@ -0,0 +1,6 @@
+const searchButton = document.querySelector('.search-button');
+const searchBar = document.getElementById('search-bar');
+
+searchButton.addEventListener('click', () => {
+    searchBar.focus(); // Focus vào thanh tìm kiếm để nó mở rộng
+});
---
+++ front end/example page/script.js
@@ -0,0 +1,9 @@
+const searchButton = document.querySelector('.search-button');
+const searchBar = document.getElementById('search-bar');
+
+searchButton.addEventListener('click', () => {
+    searchBar.focus(); // Focus vào thanh tìm kiếm để nó mở rộng
+});
+
+
+
